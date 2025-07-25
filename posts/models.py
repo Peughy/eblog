08154040af95abcdbff1e9e01717ed2bc6ca5ea3
Peughy <blackdevelopment237@gmail.com>
2025-07-25 17:12:09 +0100

    finish the feature to add the post by form manager

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to="image")
+    image = models.ImageField(upload_to="image", null=True)
     
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 
@@ -9,4 +9,36 @@ def index(request, *args, **kwargs):
 
 
 def create(request, *args, **kwargs):
-    return render(request, "posts/create.html", {"post": {}})
+    return render(request, "posts/create.html")
+
+
+def store(request, *args, **kwargs):
+
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    image = request.FILES.get("image")
+
+    if (
+        title is not ""
+        and title is not None
+        and content is not None
+        and content is not ""
+    ):
+        post = Post(title=title, content=content, image=image)
+        post.save()
+
+        return redirect("index")
+    else:
+        return render(
+            request,
+            "posts/create.html",
+            {
+                "messages": {
+                    "content": "Please complete all the field. Ps title and content"
+                }
+            },
+        )
+
+
+def update(request, *args, **kwargs):
+    return render()
