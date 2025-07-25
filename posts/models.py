7e0e8fdb8aef8885c3865281e820f07409fa0ba4
Peughy <blackdevelopment237@gmail.com>
2025-07-25 16:28:28 +0100

    finish the feature for display post for manager fro the db

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to="image")
+    
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def index(request, *args, **kwargs):
-    return render(request, "index.html")
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts": posts})
 
 
 def create(request, *args, **kwargs):
